85f14b3255cbfb0defaf77cdbe2304752adfa23c
Pradipa Y <merakayamsstraat@gmail.com>
2024-04-01 16:26:50 +0800

    menambahkan fungsi for ke integer main untuk array

--- Struktur4/Struktur4.cpp
+++ Struktur4/Struktur4.cpp
@@ -17,6 +17,7 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs[43];
 	for (int i = 0;i < 43;i++) {
+		cout << "Data Mahasiswa Ke- " << (i + 1) << endl;
 		cout << "Nomoer Mahasiswa = ";
 		getline(cin, mhs[43].nim);
 		cout << "Nama Mahasiswa = ";
@@ -29,14 +30,18 @@ int main() {
 		cin >> mhs[43].alamat.kota;
 		cout << "Umur Mahsiswa = ";
 		cin >> mhs[43].umur;
+		cin.ignore(1, '\n');
+		cout << endl;
 	}
 	for (int i = 0;i < 43;i++) {
 		cout << endl;
+		cout << "Data Mahasiswa Ke- " << (i + 1) << endl;
 		cout << "\n NIM = " << mhs[43].nim;
 		cout << "\n Nama = " << mhs[43].nama;
 		cout << "\n Alamat = ";
 		cout << "\n Desa = " << mhs[43].alamat.desa;
 		cout << "\n Kota = " << mhs[43].alamat.kota;
 		cout << "\n Umur = " << mhs[43].umur;
+		cout << endl;
 	}
 }
