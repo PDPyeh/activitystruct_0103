9eecb98e0365022f687c8c70621f6baa93e6aec7
Pradipa Y <merakayamsstraat@gmail.com>
2024-04-01 16:11:15 +0800

    menambahkan include string agar bis menambahkan command getline

---
+++ Struktur3/Struktur3.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include <string>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomoer Mahasiswa = ";
+	(cin, mhs.nim);
+	cout << "Nama Mahasiswa = ";
+
+	cout << "Alamat Mahasiswa = ";
+	cout << "\t nama Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\t nama Kota = ";
+	cin >> mhs.alamat.kota;
+
+
+	cout << "Umur Mahsiswa = ";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM = " << mhs.nim;
+	cout << "\n Nama = " << mhs.nama;
+	cout << "\n Alamat = ";
+	cout << "\n Desa = " << mhs.alamat.desa;
+	cout << "\n Kota = " << mhs.alamat.kota;
+	cout << "\n Umur = " << mhs.umur;
+}
