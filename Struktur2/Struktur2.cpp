46e5b3b8a8ae1a65176a2e6276b89c82e758754f
Pradipa Y <merakayamsstraat@gmail.com>
2024-04-01 15:54:32 +0800

    mengubah integer main bagian alamat mahasiswa

--- Struktur2/Struktur2.cpp
+++ Struktur2/Struktur2.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
 
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
 struct Mahasiswa {
 	string nim;
 	string nama;
-	string alamat;
+	DetailAlamat alamat;
 	int umur;
-};
-
-struct DetailAlamat {
-
 };
 
 int main() {
@@ -18,14 +19,20 @@ int main() {
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa = ";
 	cin >> mhs.nama;
+
 	cout << "Alamat Mahasiswa = ";
-	cin >> mhs.alamat;
+	cout << "\t nama Desa = ";
+	cin >> mhs.alamat.desa;
+	cout << "\t nama Kota = ";
+	cin >> mhs.alamat.kota;
+
+
 	cout << "Umur Mahsiswa = ";
 	cin >> mhs.umur;
 
 	cout << endl;
 	cout << "\n NIM = " << mhs.nim;
 	cout << "\n Nama = " << mhs.nama;
-	cout << "\n Alamat = " << mhs.alamat;
+	cout << "\n Alamat = " << mhs.
 	cout << "\n Umur = " << mhs.umur;
 }
